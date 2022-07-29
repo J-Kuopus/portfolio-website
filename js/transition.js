fcb37375b0e3690c314622a25e8f4d5238d9b89f
J-Kuopus <100288236+J-Kuopus@users.noreply.github.com>
2022-07-29 14:15:47 +0200

    Created function for page fade in

---
+++ js/transition.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('main').css('display', 'none');
+  $('main').fadeIn(700);
+  /*   $('a').click(function (event) {
+    event.preventDefault();
+    linkLocation = this.href;
+    $('body').fadeOut(1000, redirectPage);
+  });
+
+  function redirectPage() {
+    window.location = linkLocation;
+  } */
+});
